d792b544db9dde8b2efcd66b271d428e657211a8
John <johnsmilga@gmail.com>
2018-09-27 15:01:49 -0700

    first message

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//get elements
+const itemList = document.querySelector(".items");
+const httpForm = document.getElementById("httpForm");
+const itemInput = document.getElementById("itemInput");
+const imageInput = document.getElementById("imageInput");
+const feedback = document.querySelector(".feedback");
+const items = document.querySelector(".items");
+const submtiBtn = document.getElementById("submitBtn");
+let editedItemID = 0;
